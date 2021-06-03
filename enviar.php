978131d8408a48aa6595d8eda6cc1dbdb5746dcf
jorgematiasyam <84931994+jorgematiasyam@users.noreply.github.com>
2021-06-03 14:50:50 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,25 @@
+// configuramos datos de email
+
+$destinatario="jorgematiasyam@gmail.com";
+
+$asunto= "Email enviado desde mi sitio";
+
+
+// cuerpo de email
+
+$cuerpo.="Nombres: ".$nombres."\n";
+
+$cuerpo.="E-mail: ".$correo."\n";
+
+$cuerpo.="Mensaje: ".$mensaje."\n";
+
+
+
+
+// enviamos el email
+
+mail($destinatario,$asunto,$cuerpo);
+
+echo $nombre, "Gracias por contactarnos!!!";		
+
+?>
